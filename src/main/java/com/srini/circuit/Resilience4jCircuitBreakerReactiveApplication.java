2c2e6d8c4b798e5bbfd3cf53a8f1bb62a5fb5b20
Srini <srinivasboini7@gmail.com>
2023-05-15 13:54:51 +0800

    Initial Commit

---
+++ src/main/java/com/srini/circuit/Resilience4jCircuitBreakerReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.srini.circuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jCircuitBreakerReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jCircuitBreakerReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srini/circuit/Resilience4jCircuitBreakerReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srini.circuit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jCircuitBreakerReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
